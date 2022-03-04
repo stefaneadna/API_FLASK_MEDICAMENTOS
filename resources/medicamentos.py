535c9d1f91c1fd3b2ddda17ba105b9daadba1741
ste <stefaneadnas@gmail.com>
2022-03-04 11:34:11 -0300

    adicionando request do path

--- resources/medicamentos.py
+++ resources/medicamentos.py
@@ -1,11 +1,23 @@
 from flask_restful import Resource,reqparse
 import pandas as pd
 import json
+from flask import request
 
 class Medicamentos(Resource):
-    def get(self):
+
+
+    def get(self):              
+        data = {
+        "laboratorio": request.args.get('laboratorio'),
+        "produto": request.args.get('produto'),
+        "registro": request.args.get('registro'),
+        "substancia": request.args.get('substancia'),
+        "tarja": request.args.get('tarja')
+        }
+
         df = pd.read_csv('TA_PRECO_MEDICAMENTO.csv', encoding="Windows-1252", sep = ';')
         df_1 = df[['PRODUTO','LABORATÓRIO','REGISTRO','SUBSTÂNCIA','APRESENTAÇÃO','TARJA']]
         result = df_1.to_json(orient="records")
         parsed = json.loads(result)
-        return parsed
+        return data
+        
