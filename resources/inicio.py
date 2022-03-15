3934ee60bc011d6610be86c5e7e0053f10ce66c0
ste <stefaneadnas@gmail.com>
2022-03-15 11:40:45 -0300

    add my name

--- resources/inicio.py
+++ resources/inicio.py
@@ -4,6 +4,7 @@ class Main(Resource):
         response_data = {
         "title": "Seja bem vindo a API de medicamentos cadastrados na ANVISA!!",
         "repositorio": "https://github.com/doissegundos/API_FLASK_MEDICAMENTOS",
-        "Dataset": "https://dados.gov.br/dataset/preco-de-medicamentos-no-brasil-consumidor"
+        "Dataset": "https://dados.gov.br/dataset/preco-de-medicamentos-no-brasil-consumidor",
+        "Developer": "Stefane Adna dos Santos"
         }
         return response_data;
